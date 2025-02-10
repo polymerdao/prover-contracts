8cb2362dd1879775fffb985be2da0d0b56e973f3
RnkSngh <raunaksingh2.718@gmail.com>
2025-02-10 15:20:31 -0500

    add unit tests for view methods

--- test/CrossL2ProverV2.t.sol
+++ test/CrossL2ProverV2.t.sol
@@ -11,6 +11,7 @@ import {ISignatureVerifier} from "contracts/interfaces/ISignatureVerifier.sol";
 import {ICrossL2Prover} from "contracts/interfaces/ICrossL2Prover.sol";
 import {MerkleTrie} from "optimism/libraries/trie/MerkleTrie.sol";
 import {IAppStateVerifier} from "contracts/interfaces/IAppStateVerifier.sol";
+import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
 
 contract CrossL2ProverTest is SigningBase {
     using stdJson for string;
@@ -41,6 +42,31 @@ contract CrossL2ProverTest is SigningBase {
     // Test that a client update will fail if it doesn't have a valid sequencer signature
     function test_revert_invalidClentUpdateSignatureV2() public {
         vm.skip(true);
+    }
+
+    function test_validate_event_and_getters() public {
+        console2.log("calldata length", proof.length);
+        string memory expected = vm.readFile(string.concat(rootDir, "/test/payload/op-event-v2-1.json"));
+
+        proof = load_proof("/test/payload/op-proof-v2-1.hex");
+        (uint32 chainId, address addr, bytes memory topics, bytes memory data) = crossProverV2.validateEvent(proof);
+
+        uint32 expectedChainId = abi.decode(expected.parseRaw(".ChainID"), (uint32));
+        // address expectedAddress = abi.decode(expected.parseRaw(".Address"), (address));
+        assertEq(chainId, expectedChainId);
+        assertEq(addr, abi.decode(expected.parseRaw(".Event.address"), (address)));
+        bytes memory expectedTopics = abi.encodePacked(
+            abi.decode(expected.parseRaw(".Event.topics[0]"), (bytes32)),
+            abi.decode(expected.parseRaw(".Event.topics[1]"), (bytes32)),
+            abi.decode(expected.parseRaw(".Event.topics[2]"), (bytes32)),
+            abi.decode(expected.parseRaw(".Event.topics[3]"), (bytes32))
+        );
+
+        assertEq(topics, expectedTopics);
+        assertEq(data, abi.encodePacked(abi.decode(expected.parseRaw(".Event.data"), (bytes32))));
+
+        _checkInspectPolymerState(proof, expected);
+        _checkInspectLogIndentifier(proof, expected);
     }
 
     function test_validate_receipt_new_2() public {
@@ -78,4 +104,25 @@ contract CrossL2ProverTest is SigningBase {
 
     // Test trying to prove with a non-existent peptideApphash that hasn't yet been seen
     function test_revert_chain_ID() public {}
+
+    // Annoyingly add internal methods needed to avoid stack too deep - why does foundry check for stack to deep in
+    // tester contracts ? 😱
+    function _checkInspectPolymerState(bytes memory proof, string memory expected) internal {
+        (bytes32 stateRoot, uint64 height, bytes memory signature) = crossProverV2.inspectPolymerState(proof);
+        assertEq(stateRoot, abi.decode(expected.parseRaw(".StateRoot"), (bytes32)));
+        assertEq(height, abi.decode(expected.parseRaw(".PeptideHeight"), (uint64)));
+        // Base 64 instead of other way since go defaults to encoding raw bytes in base64
+        assertEq(Base64.encode(signature), expected.readString(".Signature"));
+    }
+
+    function _checkInspectLogIndentifier(bytes memory proof, string memory expected) internal {
+        (uint32 srcChain, uint64 blockNumber, uint16 receiptIndex, uint8 logIndex) =
+            crossProverV2.inspectLogIdentifier(proof);
+
+        assertEq(srcChain, abi.decode(expected.parseRaw(".ChainID"), (uint32)));
+        assertEq(blockNumber, abi.decode(expected.parseRaw(".BlockHeight"), (uint64)));
+        assertEq(receiptIndex, abi.decode(expected.parseRaw(".ReceiptIndex"), (uint16)));
+
+        assertEq(logIndex, expected.readUint(".Event.logIndex"));
+    }
 }
